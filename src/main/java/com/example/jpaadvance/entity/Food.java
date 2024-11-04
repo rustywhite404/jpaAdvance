40adbbd9451c8ee5a1f455ef97cf274ac34d06d7
rustywhite404 <rustywhite404@gmail.com>
2024-11-04 16:22:39 +0900

    @ManyToOne 양방향 및 조회 테스트

--- src/main/java/com/example/jpaadvance/entity/Food.java
+++ src/main/java/com/example/jpaadvance/entity/Food.java
@@ -3,11 +3,13 @@ package com.example.jpaadvance.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "food")
+@ToString
 public class Food {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
